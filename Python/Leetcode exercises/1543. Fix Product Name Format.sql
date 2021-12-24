0b0b924f6d5a922d1fcdbc1afeb47075a4849b74
LI LIU <lliu95877@gmail.com>
2021-12-23 21:44:27 -0500

    Delete 1543. Fix Product Name Format.sql

--- Python/Leetcode exercises/1543. Fix Product Name Format.sql
+++
@@ -1,9 +0,0 @@
-# Write your MySQL query statement below
-# product_name in lowercase without leading or trailing white spaces.
-# sale_date in the format ('YYYY-MM').
-# total the number of times the product was sold in this month.
-
-SELECT LOWER(TRIM(product_name)) AS product_name, DATE_FORMAT(sale_date, '%Y-%m') AS sale_date, COUNT(*) AS total
-FROM Sales
-GROUP BY 1, 2
-ORDER BY 1, 2
