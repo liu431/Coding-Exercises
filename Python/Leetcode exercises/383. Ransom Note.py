767bb301137a2435ad30a8d393a0a71c5b34b6dd
LI LIU <lliu95877@gmail.com>
2022-02-17 21:07:57 -0500

    Create 383. Ransom Note.py

---
+++ Python/Leetcode exercises/383. Ransom Note.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        # Construct dict for letters in magazine
+        letter_dic = {x : magazine.count(x) for x in set(magazine)}
+        for i in ransomNote:
+            # Letter that is in magazine
+            if i in letter_dic:
+                l_ct = letter_dic[i]
+                # Reduce count by 1
+                if l_ct > 0:
+                    letter_dic[i] -= 1
+                # Count 0 -> False
+                else:
+                    return False
+            # Letter that is not in magazine
+            else:
+                return False
+        return True
