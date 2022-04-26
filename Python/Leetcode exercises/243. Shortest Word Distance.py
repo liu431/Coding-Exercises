c8b2007e9c13dfe4e52bac5fd232a32d8cac34b5
LI LIU <lliu95877@gmail.com>
2022-04-25 20:59:58 -0400

    Update 243. Shortest Word Distance.py

--- Python/Leetcode exercises/243. Shortest Word Distance.py
+++ Python/Leetcode exercises/243. Shortest Word Distance.py
@@ -7,5 +7,18 @@ class Solution:
         # Find closest indexes as the shortest distance
         return min([abs(i1-i2) for i1 in w1_ind for i2 in w2_ind])
 
-        
-        
+# 4/25/22: use one-pass loop   
+ class Solution:
+    def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
+        res = len(wordsDict)
+        f1, f2 = 0, 0
+        for ind, word in enumerate(wordsDict):
+            if word == word1:
+                word1ind = ind
+                f1 = 1
+            if word == word2:
+                word2ind = ind
+                f2 = 1
+            if f1 & f2:
+                res = min(res, abs(word1ind-word2ind))
+        return res       
