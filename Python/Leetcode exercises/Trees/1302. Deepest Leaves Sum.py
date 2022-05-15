6bc659ffcdead59421b637a15892c1e71d7ff669
LI LIU <lliu95877@gmail.com>
2022-05-15 10:31:51 -0400

    Create 1302. Deepest Leaves Sum.py

---
+++ Python/Leetcode exercises/Trees/1302. Deepest Leaves Sum.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        next_level = deque([root,])
+        # traverse the tree (BFS)
+        while next_level:
+            curr_level = next_level
+            next_level = deque()
+            
+            for node in curr_level:
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+        # compute the sum required
+        return sum([node.val for node in curr_level])
+                
+
+
+        
