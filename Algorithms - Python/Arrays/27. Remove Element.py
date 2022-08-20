1a53b7058f4ef428417d1bf61d76ad2ab55e35c9
LI LIU <lliu95877@gmail.com>
2022-08-20 10:54:49 -0400

    Create 27. Remove Element.py

---
+++ Algorithms - Python/Arrays/27. Remove Element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        # get index of val in nums
+        val_ind = [i for i, v in enumerate(nums) if v == val]
+        # track num of val occurrence
+        ct = 0
+        # loop through num and modify in place
+        for i in val_ind:
+            nums.pop(i - ct)
+            nums.append('_')
+            ct += 1
+        return len(nums) - ct
