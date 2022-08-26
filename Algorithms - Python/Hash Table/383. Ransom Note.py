d1098a4344d450026ed7914de619fe8487daec8b
LI LIU <lliu95877@gmail.com>
2022-08-25 22:13:23 -0400

    Update and rename Algorithms - Python/383. Ransom Note.py to Algorithms - Python/Hash Table/383. Ransom Note.py

--- Algorithms - Python/Hash Table/383. Ransom Note.py
+++ Algorithms - Python/Hash Table/383. Ransom Note.py
@@ -2,6 +2,7 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         # Construct dict for letters in magazine
         letter_dic = {x : magazine.count(x) for x in set(magazine)}
+        # Or: letter_dic = collections.Counter(magazine)
         for i in ransomNote:
             # Letter that is in magazine
             if i in letter_dic:
