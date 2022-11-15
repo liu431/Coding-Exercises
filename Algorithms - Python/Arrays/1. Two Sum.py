f17cc3e395120da59b3a65da2ba16c7ebbc8a657
LI LIU <lliu95877@gmail.com>
2022-11-15 10:23:14 -0500

    Update 1. Two Sum.py

--- Algorithms - Python/Arrays/1. Two Sum.py
+++ Algorithms - Python/Arrays/1. Two Sum.py
@@ -18,7 +18,7 @@ class Solution:
 
 # Time complexity: O(n). We traverse the list containing nnn elements only once. Each lookup in the table costs only O(1) time.
 
-# Space complexity: O(n). The extra space required depends on the number of items stored in the hash table, which stores at most nnn elements.
+# Space complexity: O(n). The extra space required depends on the number of items stored in the hash table, which stores at most n elements.
 
 ## Simplier code:
 class Solution:
