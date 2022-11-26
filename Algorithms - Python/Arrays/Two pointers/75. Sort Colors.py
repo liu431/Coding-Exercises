156260b3afa757190808693fbe2e9b13de90364b
LI LIU <lliu95877@gmail.com>
2022-11-26 10:08:07 -0500

    Update 75. Sort Colors.py

--- Algorithms - Python/Arrays/Two pointers/75. Sort Colors.py
+++ Algorithms - Python/Arrays/Two pointers/75. Sort Colors.py
@@ -16,3 +16,26 @@ class Solution:
                     pass
                 nums.insert(index, current)
                 nums.pop(i + 1)
+
+                
+# re-construct teh arry with counts
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums)==1:
+            return nums
+        m0 = nums.count(0)
+        m1 = m0 + nums.count(1)
+        m2 = m1 + nums.count(2)
+        j = 0
+        while(j < len(nums)):
+            if j < m0:
+                nums[j] = 0
+            elif j < m1:
+                nums[j] = 1
+            elif j < m2:
+                nums[j] = 2
+            j += 1
+        return nums
