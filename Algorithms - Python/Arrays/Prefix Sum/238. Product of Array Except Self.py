052d8fe7b9d00b2a41c73599337e4a51d2cf6942
LI LIU <lliu95877@gmail.com>
2023-01-08 09:42:55 -0500

    Update 238. Product of Array Except Self.py

--- Algorithms - Python/Arrays/Prefix Sum/238. Product of Array Except Self.py
+++ Algorithms - Python/Arrays/Prefix Sum/238. Product of Array Except Self.py
@@ -20,17 +20,31 @@ class Solution:
         lens = len(nums)
         output = [1] * lens
         
+        # M1: two passes from left and right
         # multiply by the left side
-        left_product = 1
-        for i in range(1, lens):
-            left_product *= nums[i-1]
-            output[i] = left_product
+#         left_product = 1
+#         for i in range(1, lens):
+#             left_product *= nums[i-1]
+#             output[i] = left_product
         
-        # multiply by the right side
-        right_product = 1
-        for i in range(lens-2, -1, -1):
-            right_product *= nums[i+1]
-            output[i] *= right_product
+#         # multiply by the right side
+#         right_product = 1
+#         for i in range(lens-2, -1, -1):
+#             right_product *= nums[i+1]
+#             output[i] *= right_product
+            
+        # M2: use list comprehension (time limit exceeded because of O(n^2))
+        # for i, j in enumerate(nums):
+        #     res = [res[k] * j if k != i else res[k] for k in range(lens) ]
+        # return res
+        
+        # M3: one pass and multiply from both ends
+        left, right = 1, 1
+        for i in range(len(nums)):
+            res[i] *= left
+            res[-1-i] *= right
+            left *= nums[i]
+            right *= nums[-1-i]
         
         return output
         
