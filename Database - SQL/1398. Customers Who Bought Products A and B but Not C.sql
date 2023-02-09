908b0b43e9a1942a7fdf9f5608721c938086a450
LI LIU <lliu95877@gmail.com>
2023-02-09 17:56:34 -0500

    Update 1398. Customers Who Bought Products A and B but Not C.sql

--- Database - SQL/1398. Customers Who Bought Products A and B but Not C.sql
+++ Database - SQL/1398. Customers Who Bought Products A and B but Not C.sql
@@ -9,3 +9,18 @@ SELECT AGG.customer_id, customer_name
 FROM AGG 
 JOIN Customers ON Customers.customer_id = AGG.Customer_id
 WHERE allproducts LIKE '%A%'  AND allproducts LIKE '%B%' AND allproducts NOT LIKE '%C%' 
+
+
+
+
+# Write your MySQL query statement below
+select 
+    distinct customer_id, 
+    customer_name 
+from Orders
+left join Customers using (customer_id)
+where product_name = 'A'
+and customer_id IN 
+(select customer_id from Orders where product_name = 'B')
+and customer_id NOT IN 
+(select customer_id from Orders where product_name = 'C')
