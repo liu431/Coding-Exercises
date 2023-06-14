2c8ef6dcd844709ba3533c6f5ec577ef81f80692
LI LIU <lliu95877@gmail.com>
2023-06-13 21:57:01 -0400

    Create 530. Minimum Absolute Difference in BST.py

---
+++ Algorithms - Python/Trees/Breadth first search /530. Minimum Absolute Difference in BST.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        result = []
+        queue = [root]
+
+        while queue:
+            level_size = len(queue)
+            current_level = []
+
+            for _ in range(level_size):
+                node = queue.pop(0)
+                current_level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            result += current_level
+        result.sort()
+        diff = [abs(result[i+1] - result[i]) for i in range(0, len(result) - 1)]
+
+        return min(diff)
